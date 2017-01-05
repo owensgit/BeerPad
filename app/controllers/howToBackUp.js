9190690f07ce176c1be9e9b951bc2ada5612a0c6
Owen Evans <owensinbox@gmail.com>
2017-01-05 18:48:52 +0000

    styling of how to back up window and close button

--- app/controllers/howToBackUp.js
+++ app/controllers/howToBackUp.js
@@ -1,12 +1,14 @@
+function closeWindow() {
+    $.howToBackUpWin.close();
+}
+
 $.content.addEventListener('beforeload', function(e) {
     if (e.navigationType == Titanium.UI.iOS.WEBVIEW_NAVIGATIONTYPE_LINK_CLICKED) {
-
-       // stop the event
-       e.bubble = false;
-
-       // stop the url from loading
-       $.content.stopLoading();
-
-       // open in safari
-      Ti.Platform.openURL(e.url);
-  }});
+        // stop the event
+        e.bubble = false;
+        // stop the url from loading
+        $.content.stopLoading();
+        // open in safari
+        Ti.Platform.openURL(e.url);
+    }
+});
