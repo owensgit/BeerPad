19dfe539c95c95e59c299f9a2afd7336ff5b0900
Owen Evans <owensinbox@gmail.com>
2017-01-26 07:59:34 +0000

    clean up code in addBeer.js and add more comments

--- app/controllers/addBeer.js
+++ app/controllers/addBeer.js
@@ -1,3 +1,7 @@
+/*
+ * @file Manages adding or editing a beer
+ * @author Owen Evans
+ */
 var moment = require('alloy/moment');
 var utils = require("/utils");
 var datePicker = require("/datePicker");
@@ -5,44 +9,31 @@ var data = require("data");
 var lookUpView = require('lookUpView');
 var reviewPrompt = require('reviewPrompt');
 
+// Get args and beers collection
 var args = arguments[0] || {};
 var theBeers = Alloy.Collections.beers;
 theBeers.fetch();
 
-
-// will hold moment.js date object used for editing/saving the beer
-var the_date;
-
-
-// Add look up views the screen
-
+// Add beer look-up view to the screen ready to be used
 var beerLookUpView = new lookUpView();
 beerLookUpView.init();
 $.nameBox.add(beerLookUpView.view);
 
+// Add location look-up view to the screen ready to be used
 var locationLookUpView = new lookUpView();
 locationLookUpView.init();
 $.locationBox.add(locationLookUpView.view);
 
-
-
-
-// Array of Star Images for use in applyRating() below
-
-var starArray = [$.star1, $.star2, $.star3, $.star4, $.star5];
-
-
-// Initial placeholder values
-
-var theImage;                   // image returned from camreaMethods
-var rating;                     // rating set by rating stars
-var coords;                     // obj to store longitude/lattitude values  
+// Initial place holder values
+var theDate;                 // will hold moment.js date object
+var theImage;                 // image returned from camreaMethods
+var rating;                   // rating set by rating stars
+var coords;                   // obj to store longitude/lattitude values  
 var locSearchResults = null;
 var beerSearchResults = null;
+var ratingStarArray = [$.star1, $.star2, $.star3, $.star4, $.star5];
 
-
-// Set hint text on text fields   
-   
+// Set hint text on text fields
 $.name.hintText = L('add_name');
 $.brewery.hintText = L('add_brewery');
 $.percent.hintText = L('add_percent');
@@ -51,11 +42,48 @@ $.location.hintText = L('add_location');
 
 
 
-// Edit Mode
+/*
+ * Additional set up depending on:
+ * -------------------------------
+ *  - If adding a new beer
+ *  - editing an existing beer
+ *  - adding a beer from search
+ */
 
+// Adding a new beer
+if (!args.edit && !args.addingFromSearch) {
+    theDate = moment(); // set as the current time/date
+    applyRating(0);
+    updateDateAndTimeLabels();
+}
+
+// Editing an exiting beer
+if (args.edit) {
+    var editBeer = theBeers.where({"alloy_id": args.alloy_id})[0];    
+    applyRating(args.rating === null ? 0 : args.rating);
+    var beerImage = Alloy.Globals.getImage(args);
+    $.beerImage.image = beerImage;
+    $.title.text = L("edit_title") || "";
+
+    theDate = moment.utc(parseInt(args.date));
+    
+    updateDateAndTimeLabels();
+    $.addBeerButton.title = "Save beer";
+
+    $.dateAndTimeBox.show();
+    $.dateAndTimeBox.setTop(25);
+    $.dateAndTimeBox.setHeight(Ti.UI.SIZE);
+}
+
+// Adding a beer from search
+if (args.addingFromSearch) {
+    theDate = moment(); // set as the current time/date
+    applyRating(0);
+    $.beerImage.image = args.beer_image || args.brewery_image || null;      
+}
+
+// If editing or adding from search, populate the existing details
 if (args.edit || args.addingFromSearch) {
-    var editBeer = theBeers.where({"alloy_id": args.alloy_id})[0];
-           
     $.name.value = args.name || "";
     $.brewery.value = args.brewery || "";
     $.percent.value = args.percent || "";
@@ -65,35 +93,9 @@ if (args.edit || args.addingFromSearch) {
     if (args.notes) $.notes.value = args.notes;   
     
     $.cameraImage.opacity = 0.7;
-
-    $.dateAndTimeBox.show();
-    $.dateAndTimeBox.setTop(25);
-    $.dateAndTimeBox.setHeight(Ti.UI.SIZE);
-} else {
-	applyRating(0);
 }
 
-if (args.edit) {	
-	applyRating(args.rating === null ? 0 : args.rating);
-	var beerImage = Alloy.Globals.getImage(args);
-    $.beerImage.image = beerImage;
-    $.title.text = L("edit_title") || "";
 
-    the_date = moment.utc(parseInt(args.date));
-    
-    updateDateAndTimeLabels();
-    $.addBeerButton.title = "Save beer";
-} else {
-	// set current date
-	the_date = moment();
-
-	updateDateAndTimeLabels();
-}
-
-if (args.addingFromSearch) {
-	applyRating(0);
-	$.beerImage.image = args.beer_image || args.brewery_image || null;		
-}
 
 
 
@@ -131,7 +133,7 @@ $.addBeerButton.addEventListener("click", function () {
     // saving of the beer
     
     if (args.edit) {
-        var updatedData = utils.mapLabelsToNewArgs($, args, rating, coords, the_date.valueOf());
+        var updatedData = utils.mapLabelsToNewArgs($, args, rating, coords, theDate.valueOf());
         editBeer.set(updatedData);
         var shouldSetImage = theImage ? true : false;
         editBeer.save();
@@ -146,7 +148,7 @@ $.addBeerButton.addEventListener("click", function () {
         });           
     
     } else {
-        var beer = Alloy.createModel('beers', utils.mapLabelsToNewArgs($, args, rating, coords, the_date.valueOf()));
+        var beer = Alloy.createModel('beers', utils.mapLabelsToNewArgs($, args, rating, coords, theDate.valueOf()));
         beer.save();           
         theBeers.add(beer);
         
@@ -157,13 +159,7 @@ $.addBeerButton.addEventListener("click", function () {
         if (theImage) {
             Alloy.Globals.saveImage(beer.get('alloy_id'), theImage);  
             theImage = null;
-        }  
-        
-        //Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.beer_added, value: 1 }); 
-        
-        if (args.addingFromSearch) {
-        	//Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.lookup_beer_added, value: 1 });
-        }     
+        }    
     }
     
     this.touchEnabled = true;
@@ -176,84 +172,15 @@ $.addBeerButton.addEventListener("click", function () {
     
 });
 
-function updateDateAndTimeLabels() {
-    $.date.text = the_date.format('DD MMM YYYY');
-    $.time.text = the_date.format('HH:mm');
-}
-
-
-/**
- *  Change Date
- *  -----------
- */
-$.dateBox.addEventListener('click', function(e) {
-    var picker = new datePicker();
-    var pickerView = picker.getPicker({
-        props: {
-            type: Ti.UI.PICKER_TYPE_DATE,
-            value: the_date.toDate()
-        },
-        onCancel: function () {
-            picker.animateOut(function (e, thePicker) {
-                $.scrollView.setTouchEnabled(true);
-                $.addBeerWin.remove(pickerView);
-                picker = null;
-            });
-        },
-        onDone: function (e,thePicker) {
-            var result = thePicker.getValue();
-            the_date.date(result.getDate());
-            the_date.month(result.getMonth());
-            the_date.year(result.getFullYear());
-            updateDateAndTimeLabels();
-            picker.animateOut(function () {
-                $.scrollView.setTouchEnabled(true);
-                $.addBeerWin.remove(pickerView);
-                picker = null;
-            });
-        }
-    });
-    $.scrollView.setTouchEnabled(false);
-    $.addBeerWin.add(pickerView);
-    picker.animateIn();
-}); 
-
 
 /*
- * Change Time
- * -----------
+ * Updates the date and time labels according the theDate
+ * object in the correct format.
  */
-$.timeBox.addEventListener('click', function(e) {
-    var picker = new datePicker();
-    var dateValue = moment.utc(the_date);
-    var pickerView = picker.getPicker({
-        props: {
-            type: Ti.UI.PICKER_TYPE_TIME,
-            value: dateValue.toDate()
-        },
-        onCancel: function () {
-            picker.animateOut(function (e, thePicker) {
-                $.scrollView.setTouchEnabled(true);
-                $.addBeerWin.remove(pickerView);
-                picker = null;
-            });
-        },
-        onDone: function (e,thePicker) {
-            var result = thePicker.getValue();
-            the_date.hours(result.getHours());
-            the_date.minutes(result.getMinutes());
-            updateDateAndTimeLabels();
-            picker.animateOut(function () {
-                $.scrollView.setTouchEnabled(true);
-                $.addBeerWin.remove(pickerView);
-                picker = null;
-            });
-        }
-    });
-    $.scrollView.setTouchEnabled(false);
-    $.addBeerWin.add(pickerView);
-    picker.animateIn();
-});
+function updateDateAndTimeLabels() {
+    $.date.text = theDate.format('DD MMM YYYY');
+    $.time.text = theDate.format('HH:mm');
+}
 
 
 
@@ -341,8 +268,6 @@ function doBeerLookUp(textField) {
                 return;
             }
             
-            //Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.add_beer_api_request, value: 1 });
-            
             beerLookUpView.update(results);
             $.beerLookUpActivity.hide();
             
@@ -353,8 +278,6 @@ function doBeerLookUp(textField) {
                     $.brewery.value = e.rowData.brewery || "";
                     $.percent.value = e.rowData.abv || "";
                     $.notes.value = e.rowData.notes || "";
-                    
-                    //Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.add_beer_api_used, value: 1 });
                 }, 100);
             });
         });
@@ -394,7 +317,6 @@ $.location.addEventListener("focus", function (e) {
 $.location.addEventListener("blur", function (e) {
     locationLookUpView.closeUp();    
 });
-
 function doLocationLookUp(e) {
     var searchTerm = e.value;
     
@@ -403,29 +325,20 @@ function doLocationLookUp(e) {
     };
 
     if (searchTerm.length >= 3) { 
-        
-        $.locLookUpActivity.show();
-                 
-        data.lookUpAddress(searchTerm, function (results) {
-            
+        $.locLookUpActivity.show();     
+        data.lookUpAddress(searchTerm, function (results) {    
             if (!results) {
                 $.locLookUpActivity.hide();
                 return;
             }
-            
             locationLookUpView.update(results);
             $.locLookUpActivity.hide();
-            
-            //Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.add_beer_geocoding_request, value: 1 });
-            
             locationLookUpView.table.addEventListener("click", function (e) {
                 locationLookUpView.closeUp();
                 $.location.value = e.rowData.title;
-                coords = e.rowData.coords; 
-                //Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.add_beer_geocoding_used, value: 1 });
+                coords = e.rowData.coords;
             });
         });
-        
     } else {
         locationLookUpView.closeUp(); 
         $.locLookUpActivity.hide();   
@@ -440,8 +353,12 @@ $.location.addEventListener("change", function (e) {
 });
 
 
-// On click event for Use Location Services
 
+/*
+ * Use Location Button
+ * -------------------
+ * Function invoked when 'use location' button is pressed
+ */
 function useGPS() {
     
     locationLookUpView.closeUp();
@@ -468,7 +385,6 @@ function useGPS() {
                         var p = e.places[0];
                         $.location.value = p.street + ", " + p.city + ", " + p.country;
                         $.location.height = Ti.UI.SIZE;
-                        //Alloy.Globals.GA.trackEvent({ category: "AppEvent", action: Alloy.CFG.GA.add_beer_gps_used, value: 1 });
                         //$.locationLabel.hide();
                     } else {
                         alert(L("location_services_not_found"));
@@ -490,27 +406,106 @@ function useGPS() {
  * Apply rating
  * ------------
  */
+
+// Stores the rating (0-5) and applies it visually to the rating stars
 function applyRating(number) {
     rating = number;
     for (var i = 0; i < number; i++) {
-        starArray[i].color = Alloy.CFG.colour.main;
+        ratingStarArray[i].color = Alloy.CFG.colour.main;
     }
     for (var i = number; i < 5; i++) {
-        starArray[i].color = "#e6e6e5";
+        ratingStarArray[i].color = "#e6e6e5";
     }
 }
 
+// Click event listeners for the rating star images
 for (var i = 0; i < 5; i++) {
-   starArray[i].addEventListener("click", function () {
-       applyRating(starArray.indexOf(this) + 1);
+   ratingStarArray[i].addEventListener("click", function () {
+       applyRating(ratingStarArray.indexOf(this) + 1);
    }); 
 };
 
 
 
+/**
+ *  Change Date
+ *  -----------
+ */
+$.dateBox.addEventListener('click', function(e) {
+    var picker = new datePicker();
+    var pickerView = picker.getPicker({
+        props: {
+            type: Ti.UI.PICKER_TYPE_DATE,
+            value: theDate.toDate()
+        },
+        onCancel: function () {
+            picker.animateOut(function (e, thePicker) {
+                $.scrollView.setTouchEnabled(true);
+                $.addBeerWin.remove(pickerView);
+                picker = null;
+            });
+        },
+        onDone: function (e,thePicker) {
+            var result = thePicker.getValue();
+            theDate.date(result.getDate());
+            theDate.month(result.getMonth());
+            theDate.year(result.getFullYear());
+            updateDateAndTimeLabels();
+            picker.animateOut(function () {
+                $.scrollView.setTouchEnabled(true);
+                $.addBeerWin.remove(pickerView);
+                picker = null;
+            });
+        }
+    });
+    $.scrollView.setTouchEnabled(false);
+    $.addBeerWin.add(pickerView);
+    picker.animateIn();
+}); 
+
+
+
 /*
- * Misc
- * ----
+ * Change Time
+ * -----------
+ */
+$.timeBox.addEventListener('click', function(e) {
+    var picker = new datePicker();
+    var dateValue = moment.utc(theDate);
+    var pickerView = picker.getPicker({
+        props: {
+            type: Ti.UI.PICKER_TYPE_TIME,
+            value: dateValue.toDate()
+        },
+        onCancel: function () {
+            picker.animateOut(function (e, thePicker) {
+                $.scrollView.setTouchEnabled(true);
+                $.addBeerWin.remove(pickerView);
+                picker = null;
+            });
+        },
+        onDone: function (e,thePicker) {
+            var result = thePicker.getValue();
+            theDate.hours(result.getHours());
+            theDate.minutes(result.getMinutes());
+            updateDateAndTimeLabels();
+            picker.animateOut(function () {
+                $.scrollView.setTouchEnabled(true);
+                $.addBeerWin.remove(pickerView);
+                picker = null;
+            });
+        }
+    });
+    $.scrollView.setTouchEnabled(false);
+    $.addBeerWin.add(pickerView);
+    picker.animateIn();
+});
+
+
+
+/*
+ * Closing this window
+ * -------------------
  */
 $.addBeerWin.addEventListener("close", function() {
     $.destroy();
